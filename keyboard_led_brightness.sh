20fd6673b0a1df03cd3b1205c0d955876e5f0bbc
spewak <spewak@o2.pl>
2023-02-12 17:20:13 +0100

    Update keyboard_led_brightness.sh

--- keyboard_led_brightness.sh
+++ keyboard_led_brightness.sh
@@ -8,8 +8,9 @@ MODDIR=${0%/*}
 
 LCD_BRIGHTNESS_FILE=/sys/class/leds/lcd-backlight/brightness
 
+for i in 1 2 3 4 5 6
+do
 LCD_VALUE=$(cat ${LCD_BRIGHTNESS_FILE})
-
 
 # Enable keyboard backlight if LCD is on. Led values can range between 0 and 6. led2 increases brightness further
 if [[ ${LCD_VALUE} != '0' && -d "/sys/class/leds/mt6370_pmu_led1" ]]; then
@@ -32,4 +33,14 @@ elif [[ ${LCD_VALUE} == '0' && -d "/sys/class/leds/button-backlight" ]]; then
 	echo 0 > /sys/class/leds/button-backlight/brightness
 	
 fi
+sleep 10
+
+         LCD_VALUE=$(cat ${LCD_BRIGHTNESS_FILE}) 
+	 
+if [[ ${LCD_VALUE} == '0' ]]; then
+     
+	 echo 0 > /sys/class/leds/button-backlight/brightness
+         break
+fi
+done
 exit 0
